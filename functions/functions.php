30870a39ea569674545ae317f111b01243b474d8
sait0928 <sait0yuya928@gmail.com>
2019-04-22 14:40:16 +0900

    Add files via upload

--- functions/functions.php
+++ functions/functions.php
@@ -6,12 +6,13 @@ function h($string)
   return htmlspecialchars($string, ENT_QUOTES, 'utf-8');
 }
 
-function insertParentTask($user_task, $user_name) {
+function insertParentTask($user_task, $user_name, $date) {
   $pdo = connect();
-  $stmt = $pdo->prepare("INSERT INTO tasks (content, users_name, achieve) VALUES (:content, :users_name, 'still')");
+  $stmt = $pdo->prepare("INSERT INTO tasks (content, users_name, achieve, deadline) VALUES (:content, :users_name, 'still', :deadline)");
 
   $stmt->bindParam(':content', $user_task, PDO::PARAM_STR);
   $stmt->bindParam(':users_name', $user_name, PDO::PARAM_STR);
+  $stmt->bindParam(':deadline', $date, PDO::PARAM_STR);
 
   return $stmt->execute();
 }
@@ -19,7 +20,7 @@ function insertParentTask($user_task, $user_name) {
 function selectParentTasks($user_name) {
   $pdo = connect();
   // 条件複数指定はAND,NULLの検索には=NULLではなくISNULLを使う
-  $stmt = $pdo->prepare("SELECT * FROM tasks WHERE parent_id IS NULL AND achieve = 'still' AND users_name = :users_name");
+  $stmt = $pdo->prepare("SELECT * FROM tasks WHERE parent_id IS NULL AND achieve = 'still' AND users_name = :users_name ORDER BY deadline ASC");
 
   $stmt->bindParam(':users_name', $user_name, PDO::PARAM_STR);
 
@@ -89,6 +90,27 @@ function transferTasks($id_arr, $achieve) {
   $stmt = $pdo->query($sql);
 }
 
+function conversionChildTasks($id_arr) {
+  foreach($id_arr as $id) {
+    $pdo = connect();
+    $stmt = $pdo->prepare("SELECT * FROM tasks WHERE id = :id");
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+    if($row['achieve'] === 'still') {
+      $sql = "UPDATE tasks SET achieve = 'already' WHERE id = :id";
+    }else {
+      $sql = "UPDATE tasks SET achieve = 'still' WHERE id = :id";
+    }
+
+    $pdo = connect();
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+  }
+}
+
 function deleteTasks($id_arr) {
   $pdo = connect();
   $i = 0;
@@ -134,4 +156,12 @@ function register($user_name, $password) {
   $stmt->bindParam(':password', $password, PDO::PARAM_STR);
 
   return $stmt->execute();
+}
+
+function dateDiff($date, $deadline) {
+  $date_ts = strtotime($date);
+  $deadline_ts = strtotime($deadline);
+  $second_diff = $deadline_ts - $date_ts;
+  $date_diff = $second_diff / (60 * 60 * 24);
+  return $date_diff;
 }
