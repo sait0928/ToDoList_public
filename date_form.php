cb391534dbbe009df73fc561d65a01ae38a92592
sait0928 <sait0yuya928@gmail.com>
2019-05-13 12:11:38 +0900

    Add files via upload

--- date_form.php
+++ date_form.php
@@ -2,7 +2,7 @@
   <select name="year" class="js-changeYear">
     <?php foreach(range($current_year,$current_year+4) as $year): ?>
     <option value="<?php echo $year; ?>"><?php echo $year; ?></option>
-    <?php endforeach ?>
+    <?php endforeach; ?>
   </select>
   <select name="month" class="js-changeMonth">
     <?php foreach(range(1,12) as $month): ?>
